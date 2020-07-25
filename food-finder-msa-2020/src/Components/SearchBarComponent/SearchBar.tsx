999158f0540436ce22d5a889e2a3ee7a52abb4b0
Azizul <misl000@aucklanduni.ac.nz>
2020-07-25 14:14:15 +1200

    Added Search Bar Component

--- food-finder-msa-2020/src/Components/SearchBarComponent/SearchBar.tsx
+++ food-finder-msa-2020/src/Components/SearchBarComponent/SearchBar.tsx
@@ -1,18 +1,31 @@
 import React, { useState } from "react"
-import { Grid, TextField } from "@material-ui/core"
+import { Button, Grid, TextField } from "@material-ui/core"
 import "./SearchBar.css"
 
 interface ISearchBarProps {
-  SetUserInput: (a: String) => void
+  SetUserInput: (userInput: String | null) => void
 }
 
-function SearchBar() {
+function SearchBar(props: ISearchBarProps) {
   const [SearchQuery, setSearchQuery] = useState<string | null>("")
-  const handleSearchQueryChange = (s: string | null) => {
-    setSearchQuery(s)
+  const handleSearchQueryChange = (inputString: string | null) => {
+    setSearchQuery(inputString)
   }
-
   const [HasFocus, setHasFocus] = useState<boolean>(false)
+
+  const handleSubmit = () => {
+    console.log(SearchQuery)
+    if (
+      SearchQuery?.length !== 0 &&
+      SearchQuery !== null &&
+      SearchQuery !== ""
+    ) {
+      let UserInput: String = SearchQuery
+      props.SetUserInput(UserInput)
+    } else {
+      setHasFocus(true)
+    }
+  }
 
   return (
     <div className="SearchBarContainer">
@@ -28,6 +41,12 @@ function SearchBar() {
             value={SearchQuery}
             onChange={(e) => handleSearchQueryChange(e.target.value)}
           />
+        </Grid>
+
+        <Grid item xs={6} sm={3}>
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
         </Grid>
       </Grid>
     </div>
